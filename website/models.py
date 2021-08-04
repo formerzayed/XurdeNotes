bf341de018a7cf613c81a4747dcf55944f7200fd
DevXurde <thexurde123@gmail.com>
2021-08-04 11:21:31 +0530

    Models + Views + Database

--- website/models.py
+++ website/models.py
@@ -8,7 +8,7 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     title = db.Column(db.String)
-    note = db.Column(db.String)
+    description = db.Column(db.String)
 
     date = db.Column(db.DateTime, default=datetime.utcnow())
 
--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect, flash
 from . import open_settings
 from flask_login import login_required, current_user
 
-# VIEWS-BLUEPRINT
+# views blueprint
 views = Blueprint(name="views", import_name=__name__)
 
 # opening settings
